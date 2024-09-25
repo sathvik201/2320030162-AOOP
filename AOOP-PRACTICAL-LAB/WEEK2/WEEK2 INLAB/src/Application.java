8cb789680f5d3dfc6911f6d796725c523007c06f
M Sathvik Reddy <msathvikreddy2006@gmail.com>
2024-09-25 10:46:13 +0530

    Add files via upload

---
+++ AOOP-PRACTICAL-LAB/WEEK2/WEEK2 INLAB/src/Application.java
@@ -0,0 +1,13 @@
+package com.DP;
+
+public class Application {
+	 public static void main(String[] args) {
+		 // Get the logger instance
+		 Logger1 logger = Logger1.getInstance();
+		 // Log some messages
+		 logger.log("Application started.");
+		 logger.log("Performing some operations...");
+		 logger.log("Application finished successfully.");
+		 }
+		
+}
